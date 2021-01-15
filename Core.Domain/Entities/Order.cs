f85e8bef6dc65ca38774f1a6f88fc444754d5af9
Dev <manyazehal@gmail.com>
2021-01-15 16:09:32 +0300

    AuditableEntitiy ,Category,Event,Order Class  Added

---
+++ Core.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RootApi.Domain.Common;
+
+namespace Core.Domain.Entities
+{
+    public class Order:AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+        
+        
+    }
+}
